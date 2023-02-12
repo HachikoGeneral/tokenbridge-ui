d3c65080378d7fceb187c0026c6d2db00da99a28
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-12 18:03:06 +0000

    Update rpc.js

--- src/constants/rpc.js
+++ src/constants/rpc.js
@@ -1,26 +1,22 @@
 import * as chainId from './chainId'
 import {
   TEST_NET_KOVAN_CONFIG,
-  TEST_NET_RSK_CROSS_KOVAN_CONFIG,
   MAIN_NET_ETH_CONFIG,
-  MAIN_NET_RSK_CONFIG,
 } from './networks'
 
 const sideChainIdStr = process.env.VUE_APP_SIDE_CHAIN_ID
 const mainChainIdStr = process.env.VUE_APP_MAIN_CHAIN_ID
 
-const sideChainId = sideChainIdStr ? parseInt(sideChainIdStr, 10) : chainId.MAIN_NET_ETHEREUM
-const mainChainId = mainChainIdStr ? parseInt(mainChainIdStr, 10) : chainId.MAIN_NET_RSK
+const sideChainId = sideChainIdStr ? parseInt(sideChainIdStr, 10) : chainId.MAIN_NET_KOVAN
+const mainChainId = mainChainIdStr ? parseInt(mainChainIdStr, 10) : chainId.MAIN_NET_POLYGON
 
 // --------- CONFIGS ----------
 export const TEST_NET_RPC = {
   [chainId.TEST_NET_KOVAN]: TEST_NET_KOVAN_CONFIG.rpc,
-  [chainId.TEST_NET_RSK]: TEST_NET_RSK_CROSS_KOVAN_CONFIG.rpc,
 }
 
 export const MAIN_NET_RPC = {
-  [chainId.MAIN_NET_ETHEREUM]: MAIN_NET_ETH_CONFIG.rpc,
-  [chainId.MAIN_NET_RSK]: MAIN_NET_RSK_CONFIG.rpc,
+  [chainId.MAIN_NET_POLYGON]: MAIN_NET_ETH_CONFIG.rpc,
 }
 
 export const ALL_RPC = {
@@ -30,8 +26,6 @@ export const ALL_RPC = {
 
 export function getMainRPC() {
   if (!mainChainId) {
-    return MAIN_NET_RSK_CONFIG.rpc
-  }
   return ALL_RPC[mainChainId]
 }
 
