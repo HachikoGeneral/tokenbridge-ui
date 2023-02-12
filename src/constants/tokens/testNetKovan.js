5a6c07b8def3d63de2d7f59ffec5d4da84c8661c
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-12 17:39:47 +0000

    Update testNetKovan.js

--- src/constants/tokens/testNetKovan.js
+++ src/constants/tokens/testNetKovan.js
@@ -3,44 +3,14 @@ import * as methodType from '../methodType'
 
 export const TEST_NET_KOVAN_GAS_TOKEN = {
   ...tokensInfo.TOKEN_WETH_INFO,
-  symbol: 'ETH',
+  symbol: 'WCHK',
 }
 
 export const TEST_NET_KOVAN_MAIN_TOKEN = {
   ...tokensInfo.TOKEN_WETH_INFO,
-  symbol: 'ETH',
-  address: '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
+  symbol: 'WCHK',
+  address: '0x2e5E530dC2C6b2A8f214ee929dC4a302575881A9',
   decimals: 18,
   methodType: methodType.DEPOSITOR,
-}
-
-export const TEST_NET_KOVAN_TOKENS = [
-  {
-    ...tokensInfo.TOKEN_DAI_INFO,
-    symbol: 'DAI',
-    address: '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_LINK_INFO,
-    symbol: 'LINK',
-    address: '0xa36085F69e2889c224210F603D836748e7dC0088',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_USDT_INFO,
-    symbol: 'USDT',
-    address: '0x13512979ade267ab5100878e2e0f485b568328a4',
-    decimals: 6,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_BUND_INFO,
-    symbol: 'BUND',
-    address: '0x8d3e855f3f55109d473735ab76f753218400fe96',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
   },
 ]
