2b39fc3978282ef396ec6706b7674f380a0e43cd
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-12 18:06:56 +0000

    Update tokensInfo.js

--- src/constants/tokensInfo.js
+++ src/constants/tokensInfo.js
@@ -1,43 +1,17 @@
 // --------- TOKENS export --------------
 
-export const TOKEN_RBTC_INFO = {
-  token: 'RBTC',
-  name: 'Rsk BTC',
-  typeId: 4,
-  icon: '',
+export const TOKEN_WCHK_INFO = {
+  token: 'WCHK',
+  name: 'WCHK',
+  typeId: 42,
+  icon: 'https://ibb.co/dp1Lg6t',
 }
 
-export const TOKEN_DAI_INFO = {
-  token: 'DAI',
-  name: 'Dai Stablecoin',
-  typeId: 4,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+export const TOKEN_WCHK_INFO = {
+  token: 'WCHK',
+  name: 'WCHK',
+  typeId: 137,
+  icon: 'https://ibb.co/dp1Lg6t',
 }
 
-export const TOKEN_WETH_INFO = {
-  token: 'WETH',
-  name: 'Wrapped Ether',
-  typeId: 1,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-}
 
-export const TOKEN_LINK_INFO = {
-  token: 'LINK',
-  name: 'Chainlink Token',
-  typeId: 3,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x514910771AF9Ca656af840dff83E8264EcF986CA/logo.png',
-}
-
-export const TOKEN_USDT_INFO = {
-  token: 'USDT',
-  name: 'Tether USD',
-  typeId: 4,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png',
-}
-
-export const TOKEN_BUND_INFO = {
-  token: 'BUND',
-  name: 'Bundles Finance',
-  typeId: 3,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/bund.png',
-}
